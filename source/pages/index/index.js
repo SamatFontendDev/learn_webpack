c0846927fdda25ede41f40c2ac66b62e3cbd3fc1
SamatFrontendDev <samatkhabibyllin1991@gmail.com>
2019-05-19 15:34:55 +0300

    1-19.05.19

---
+++ source/pages/index/index.js
@@ -0,0 +1,4 @@
+import createMenu from '../../components/menu/menu';
+var menu = createMenu(['Главная', 'Обо мне'], 'menu');
+document.body.appendChild(menu);
+console.log('in index.js');
--- webpack.config.js
+++ webpack.config.js
@@ -5,15 +5,56 @@ const paths = {
     build: path.join(__dirname, 'build')
 };
 
-module.exports = {
-    entry: paths.source + '/index.js',
+const common = {
+    entry: {
+        'index': './source/pages/index/index.js',
+        'blog': './source/pages/blog/blog.js'
+    },
     output: {
         path: paths.build,
         filename: '[name].js'
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'Webpack app'
+            filename: 'index.html',
+            chunks: ['index'],
+            template: './source/pages/index/index.pug'
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'blog.html',
+            chunks: ['blog'],
+            template: './source/pages/blog/blog.pug'
         })
-    ]
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.pug$/,
+                loader: 'pug-loader',
+                options: {
+                    pretty: true
+                }
+            }
+        ]
+    }
+};
+
+const developmentConfig = {
+    devServer: {
+        stats: 'errors-only',
+        port: 9000
+    }
+};
+
+module.exports = function(env){
+    if(env === 'production'){
+        return common;
+    }
+    if(env === 'development'){
+        return Object.assign(
+            {},
+            common,
+            developmentConfig
+        )
+    }
 }
