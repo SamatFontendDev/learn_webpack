622f24f61b845430945b36cbab62f33b875d9215
SamatFrontendDev <samatkhabibyllin1991@gmail.com>
2019-05-20 08:15:46 +0300

    1-20.05.19

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const pug = require('./webpack/pug');
 const devserver = require('./webpack/devServer');
 const sass = require('./webpack/sass');
 const css = require('./webpack/css');
+const extractCss = require('./webpack/css.extract');
 
 const paths = {
     source: path.join(__dirname, 'source'),
@@ -19,7 +20,7 @@ const common = merge([
         },
         output: {
             path: paths.build,
-            filename: '[name].js'
+            filename: 'js/[name].js'
         },
         plugins: [
             new HtmlWebpackPlugin({
@@ -39,7 +40,10 @@ const common = merge([
 
 module.exports = function(env){
     if(env === 'production'){
-        return common;
+        return merge([
+            common,
+            extractCss()
+        ]);
     }
     if(env === 'development'){
         return merge([
